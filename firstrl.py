aa91447cfdc154b2dbe48116fac16e9082a95688
Ninjalah <latifalston@gmail.com>
2018-09-01 17:47:00 +0900

    inventory screen created, though cannot use items just yet

--- firstrl.py
+++ firstrl.py
@@ -17,6 +17,7 @@ PANEL_Y = SCREEN_HEIGHT - PANEL_HEIGHT
 MSG_X = BAR_WIDTH + 2
 MSG_WIDTH = SCREEN_WIDTH - BAR_WIDTH - 2
 MSG_HEIGHT = PANEL_HEIGHT - 1
+INVENTORY_WIDTH = 50
  
 # parameters for dungeon generator (num of monsters, items, etc.)
 ROOM_MAX_SIZE = 10
@@ -546,6 +547,10 @@ def handle_keys():
                         object.item.pick_up()
                         break
 
+            if key_char == 'i':
+                # show the inventory; if an item is selected, use it
+                inventory_menu('Press the key next to an item to use it, or any other to cancel.\n')
+
             return 'didnt-take-turn'
 
 # ends game is player dies!
@@ -568,7 +573,54 @@ def monster_death(monster):
      monster.ai = None
      monster.name = 'remains of ' + monster.name
      monster.send_to_back()
- 
+
+# define a menu window. Has a string at the top (header), list of strings (options. can be names of items, for ex), and the window's width (width)
+def menu(header, options, width):
+    if len(options) > 26: raise ValueError('Cannot have a menu with more than 26 options.')
+
+    # calculate total height for the header (after auto-wrap) and one line per option
+    header_height = libtcod.console_get_height_rect(con, 0, 0, width, SCREEN_HEIGHT, header)
+    height = len(options) + header_height
+
+    # create an off-screen console that represents the menu's window
+    window = libtcod.console_new(width, height)
+
+    # print the header, with auto-wrap
+    libtcod.console_set_default_foreground(window, libtcod.white)
+    libtcod.console_print_rect_ex(window, 0, 0, width, height, libtcod.BKGND_NONE, libtcod.LEFT, header)
+
+    # print all the options
+    y = header_height
+    letter_index = ord('a')
+    for option_text in options:
+        text = '(' + chr(letter_index) + ') ' + option_text
+        libtcod.console_print_ex(window, 0, y, libtcod.BKGND_NONE, libtcod.LEFT, text)
+        y += 1
+        letter_index += 1
+
+    # blit the contents of "window" to the root console
+    x = SCREEN_WIDTH/2 - width/2
+    y = SCREEN_HEIGHT/2 - height/2
+    libtcod.console_blit(window, 0, 0, width, height, 0, x, y, 1.0, 0.7)
+
+    # present the root console to the player and wait for a key-press
+    libtcod.console_flush()
+    key = libtcod.console_wait_for_keypress(True)
+
+    # convert the ASCII code to an index; if it corresponds to an option, return it
+    index = key.c - ord('a')
+    if index >= 0 and index < len(options): return index
+    return None
+
+# show a menu with each item of the inventory as an option
+def inventory_menu(header):
+    if len(inventory) == 0:
+        options = ['Inventory is empty.']
+    else:
+        options = [item.name for item in inventory]
+
+    index = menu(header, options, INVENTORY_WIDTH)
+
 #############################################
 # Initialization & Main Loop                #
 #############################################
